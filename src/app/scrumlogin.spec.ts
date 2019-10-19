6cbb578ce8d4c14b34cbff3fc826e62453e7eb9d
iykeevans <elochi238@gmail.com>
2019-10-19 19:43:16 +0100

    feat: implement form validity and error message display

---
+++ src/app/scrumlogin.spec.ts
@@ -0,0 +1,7 @@
+import { Scrumlogin } from './scrumlogin';
+
+describe('Scrumlogin', () => {
+  it('should create an instance', () => {
+    expect(new Scrumlogin()).toBeTruthy();
+  });
+});
---
+++ src/app/scrumlogin.ts
@@ -0,0 +1,7 @@
+export class Scrumlogin {
+  constructor(
+    public email: string,
+    public password: string,
+    public projectName: string
+  ) {}
+}
